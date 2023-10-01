db6b7d82dae2099bab408fdef00eaeff6f6fdbd5
Satya Prakash <96316675+Satya1824@users.noreply.github.com>
2023-10-01 18:54:19 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,4 @@
-import userModel from "../Models/userModel.js";
+import userModel from "../models/userModel.js";
 import { comparePassword, hashPassword } from "../helpers/authHelper.js";
 import JWT from "jsonwebtoken";
 
